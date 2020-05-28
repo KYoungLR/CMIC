4a6cce7a4cf92effce5dcc3bc12bfad271f6ef28
Kayleen Lim <kayleen.lim@liferay.com>
2020-05-28 14:50:43 -0700

    CMIC-179 Add models for Producer API

---
+++ modules/integration/cmic-rest/cmic-rest-api/src/main/java/com/churchmutual/rest/model/CMICContact.java
@@ -0,0 +1,72 @@
+package com.churchmutual.rest.model;
+
+/**
+ * @author Kayleen Lim
+ */
+public class CMICContact extends CMICObject {
+
+	public String getDepartment() {
+		return _department;
+	}
+
+	public String getEmail() {
+		return _email;
+	}
+
+	public String getFirstName() {
+		return _firstName;
+	}
+
+	public long getId() {
+		return _id;
+	}
+
+	public String getLastName() {
+		return _lastName;
+	}
+
+	public String getPhoneNumber() {
+		return _phoneNumber;
+	}
+
+	public String getSuffix() {
+		return _suffix;
+	}
+
+	public void setDepartment(String department) {
+		_department = department;
+	}
+
+	public void setEmail(String email) {
+		_email = email;
+	}
+
+	public void setFirstName(String firstName) {
+		_firstName = firstName;
+	}
+
+	public void setId(long id) {
+		_id = id;
+	}
+
+	public void setLastName(String lastName) {
+		_lastName = lastName;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		_phoneNumber = phoneNumber;
+	}
+
+	public void setSuffix(String suffix) {
+		_suffix = suffix;
+	}
+
+	private String _department;
+	private String _email;
+	private String _firstName;
+	private long _id;
+	private String _lastName;
+	private String _phoneNumber;
+	private String _suffix;
+
+}
