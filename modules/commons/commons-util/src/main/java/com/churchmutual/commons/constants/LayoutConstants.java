3ebb9f9aa85bf3aa0596bc45549270f60794a676
Matthew Chan <matthew.chan@liferay.com>
2020-05-29 10:17:50 -0700

    CMIC-206 Add portlets to dashboard page

--- modules/commons/commons-util/src/main/java/com/churchmutual/commons/constants/LayoutConstants.java
+++ modules/commons/commons-util/src/main/java/com/churchmutual/commons/constants/LayoutConstants.java
@@ -4,6 +4,10 @@ public class LayoutConstants {
 
 	public static final String COL_ID_COLUMN_1 = "column-1";
 
+	public static final String COL_ID_COLUMN_2 = "column-2";
+
 	public static final String LAYOUT_1_COLUMN = "1_column";
+
+	public static final String LAYOUT_2_COLUMNS_66_33 = "cmic_2_columns_66_33";
 
 }
--- modules/content-setup/src/main/java/com/churchmutual/content/setup/upgrade/util/broker/BrokerDashboardPage.java
+++ modules/content-setup/src/main/java/com/churchmutual/content/setup/upgrade/util/broker/BrokerDashboardPage.java
@@ -1,24 +1,47 @@
 package com.churchmutual.content.setup.upgrade.util.broker;
 
+import com.churchmutual.commons.constants.LayoutConstants;
 import com.churchmutual.commons.util.LayoutConfig;
 import com.churchmutual.commons.util.LayoutHelper;
 import com.churchmutual.content.setup.upgrade.constants.ContentSetupKeys;
+
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 
 /**
  * @author Matthew Chan
  */
 public class BrokerDashboardPage {
 
-	public static void addPage(long companyId, long userId, long groupId)
-		throws Exception {
-
+	public static void addPage(long companyId, long userId, long groupId) throws Exception {
 		LayoutConfig layoutConfig = new LayoutConfig().setName(
 			"Dashboard"
 		).setFriendlyURL(
 			ContentSetupKeys.LAYOUT_FURL_BROKER_DASHBOARD
 		);
 
-		LayoutHelper.addLayout(userId, groupId, layoutConfig);
+		Layout layout = LayoutHelper.addLayout(userId, groupId, layoutConfig);
+
+		LayoutTypePortlet layoutTypePortlet = (LayoutTypePortlet)layout.getLayoutType();
+
+		layoutTypePortlet.setLayoutTemplateId(userId, LayoutConstants.LAYOUT_2_COLUMNS_66_33);
+
+		layoutTypePortlet.addPortletIds(userId, _PORTLETS_COLUMN_1, LayoutConstants.COL_ID_COLUMN_1, true);
+
+		layoutTypePortlet.addPortletIds(userId, _PORTLETS_COLUMN_2, LayoutConstants.COL_ID_COLUMN_2, true);
+
+		LayoutLocalServiceUtil.updateLayout(
+			layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(), layout.getTypeSettings());
 	}
+
+	private static final String[] _PORTLETS_COLUMN_1 = {
+		"com_churchmutual_account_producer_dashboard_web_portlet_AccountProducerDashboardWebPortlet",
+		"com_churchmutual_commission_web_portlet_CommissionWebPortlet"
+	};
+
+	private static final String[] _PORTLETS_COLUMN_2 = {
+		"com_churchmutual_contact_producer_dashboard_web_portlet_ContactProducerDashboardWebPortlet"
+	};
 
 }
