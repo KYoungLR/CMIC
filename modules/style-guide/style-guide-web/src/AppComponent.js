773cc5bf4e0725675a23b95d196f9fc9ab480382
julienmouraddotcom <62948119+julienmouraddotcom@users.noreply.github.com>
2020-05-29 17:48:08 -0400

    CMIC-163 Add typography style guide

---
+++ modules/style-guide/style-guide-web/src/AppComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Typography from './components/typography';
+
+export default class extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Typography />
+			</React.Fragment>
+		);
+	}	
+}
