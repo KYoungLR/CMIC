cae3158143f6b0155644dd0b1033ed6925c29356
julienmouraddotcom <62948119+julienmouraddotcom@users.noreply.github.com>
2020-05-29 18:54:35 -0400

    CMIC-236 Add color scheme style guide

--- modules/style-guide/style-guide-web/src/AppComponent.js
+++ modules/style-guide/style-guide-web/src/AppComponent.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Typography from './components/typography';
+import Colors from './components/colors';
 
 export default class extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
 				<Typography />
+				<Colors />
 			</React.Fragment>
 		);
 	}	
