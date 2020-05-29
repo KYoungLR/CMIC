b509457b79e83938a798ef6e2b7e4d2fe8afd983
Kayleen Lim <kayleen.lim@liferay.com>
2020-05-29 16:20:44 -0700

    CMIC-191 Add models for Commission Document API

---
+++ modules/integration/cmic-rest/cmic-rest-api/src/main/java/com/churchmutual/rest/model/CMICCommissionDocument.java
@@ -0,0 +1,54 @@
+package com.churchmutual.rest.model;
+
+/**
+ * @author Kayleen Lim
+ */
+public class CMICCommissionDocument extends CMICObject {
+
+	public String getAgentNumber() {
+		return _agentNumber;
+	}
+
+	public String getDivsionNumber() {
+		return _divsionNumber;
+	}
+
+	public String getDocumentType() {
+		return _documentType;
+	}
+
+	public String getId() {
+		return _id;
+	}
+
+	public String getStatementDate() {
+		return _statementDate;
+	}
+
+	public void setAgentNumber(String agentNumber) {
+		_agentNumber = agentNumber;
+	}
+
+	public void setDivsionNumber(String divsionNumber) {
+		_divsionNumber = divsionNumber;
+	}
+
+	public void setDocumentType(String documentType) {
+		_documentType = documentType;
+	}
+
+	public void setId(String id) {
+		_id = id;
+	}
+
+	public void setStatementDate(String statementDate) {
+		_statementDate = statementDate;
+	}
+
+	private String _agentNumber;
+	private String _divsionNumber;
+	private String _documentType;
+	private String _id;
+	private String _statementDate;
+
+}
