b0c938e27e6a2a91e7cc764275d3ae3f4cbefa15
julienmouraddotcom <62948119+julienmouraddotcom@users.noreply.github.com>
2020-06-01 18:19:38 -0400

    CMIC-238 Add button style guide markup

--- modules/style-guide/style-guide-web/src/AppComponent.js
+++ modules/style-guide/style-guide-web/src/AppComponent.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Typography from './components/typography';
 import Colors from './components/colors';
 import Elevation from './components/elevation';
+import Buttons from './components/buttons';
 
 export default class extends React.Component {
 	render() {
@@ -12,6 +13,7 @@ export default class extends React.Component {
 				<Typography />
 				<Colors />
 				<Elevation />
+				<Buttons />
 			</React.Fragment>
 		);
 	}	
