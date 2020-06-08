1b296a8ec72497a88183e9efda4ea8eb1a422001
Kayleen Lim <kayleen.lim@liferay.com>
2020-06-08 15:45:41 -0700

    CMIC-297 Add CMICContactLocalService

---
+++ modules/cmic/cmic-core/cmic-core-service/src/main/java/com/churchmutual/core/service/impl/CMICContactLocalServiceImpl.java
@@ -0,0 +1,12 @@
+package com.churchmutual.core.service.impl;
+
+import com.churchmutual.core.service.CMICContactLocalService;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kayleen Lim
+ */
+@Component(immediate = true, service = CMICContactLocalService.class)
+public class CMICContactLocalServiceImpl implements CMICContactLocalService {
+}
