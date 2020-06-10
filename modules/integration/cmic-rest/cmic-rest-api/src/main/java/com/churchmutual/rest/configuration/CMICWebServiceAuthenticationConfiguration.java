b440c4d64f932fbec9417be57e1dcc4525fe83d5
Kayleen Lim <kayleen.lim@liferay.com>
2020-06-10 14:57:57 -0700

    CMIC-277 Add authentication settings for API Gateway

---
+++ modules/integration/cmic-rest/cmic-rest-api/src/main/java/com/churchmutual/rest/configuration/CMICWebServiceAuthenticationConfiguration.java
@@ -0,0 +1,33 @@
+package com.churchmutual.rest.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Kayleen Lim
+ */
+@ExtendedObjectClassDefinition(category = "cmic")
+@Meta.OCD(
+	description = "Configure settings for accessing API Gateway to Church Mutual REST Services.",
+	id = "com.churchmutual.rest.configuration.CMICWebServiceAuthenticationConfiguration",
+	localization = "content/Language", name = "church-mutual-web-service-authentication"
+)
+public interface CMICWebServiceAuthenticationConfiguration {
+
+	@Meta.AD(deflt = "", description = "Base URL for accessing Church Mutual REST Services")
+	public String baseURL();
+
+	@Meta.AD(deflt = "", description = "Header Key")
+	public String headerKey();
+
+	@Meta.AD(deflt = "", description = "Header Value")
+	public String headerValue();
+
+	@Meta.AD(deflt = "", description = "Username for Basic Auth")
+	public String basicAuthUsername();
+
+	@Meta.AD(deflt = "", description = "Password for Basic Auth")
+	public String basicAuthPassword();
+
+}
