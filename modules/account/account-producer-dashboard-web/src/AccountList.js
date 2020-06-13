e16110b3e268a63b6abb6861ee2cda621eb2b03a
julienmouraddotcom <62948119+julienmouraddotcom@users.noreply.github.com>
2020-06-12 20:32:13 -0400

    CMIC-258 Minor tweaks

--- modules/account/account-producer-dashboard-web/src/AccountList.js
+++ modules/account/account-producer-dashboard-web/src/AccountList.js
@@ -13,54 +13,50 @@ const AccountList = (props) => {
   }
   else {
     return (
-      <ClayTable>
-        <ClayTable.Head>
-          <ClayTable.Row>
-            <ClayTable.Cell expanded headingCell>
-              {Liferay.Language.get('name')}
-              <a href="javascript:;" className="text-muted">
-                <ClayIcon symbol={"sort"} spritemap={spritemap} />
-              </a>
-            </ClayTable.Cell>
-
-            <ClayTable.Cell expanded headingCell align="center" className="table-cell-expand-smallest">
-              {Liferay.Language.get('in-force-policies')}
-              <a href="javascript:;" className="text-muted">
-                <ClayIcon symbol={"sort"} spritemap={spritemap} />
-              </a>
-            </ClayTable.Cell>
-
-            <ClayTable.Cell headingCell align="right" className="table-cell-ws-nowrap">
-              {Liferay.Language.get('amount-billed')}
-              <a href="javascript:;" className="text-muted">
-                <ClayIcon symbol={"sort"} spritemap={spritemap} />
-              </a>
-            </ClayTable.Cell>
-
-          </ClayTable.Row>
-        </ClayTable.Head>
-        <ClayTable.Body>
-          {props.accountsList.map((account, index) => (
-            <ClayTable.Row key={index}>
-              <ClayTable.Cell>
-                <h5 className="font-weight-bold mb-0">{account.accountName}</h5>
-                <small className="text-muted">#{account.accountNumber}</small>
+      <React.Fragment>
+        <ClayTable>
+          <ClayTable.Head>
+            <ClayTable.Row>
+              <ClayTable.Cell expanded headingCell>
+                {Liferay.Language.get('name')}
+                <a href="javascript:;" className="text-muted">
+                  <ClayIcon symbol={"sort"} spritemap={spritemap} />
+                </a>
               </ClayTable.Cell>
-              <ClayTable.Cell align="center">{account.inForcePolicies}</ClayTable.Cell>
-              <ClayTable.Cell align="right" className="h3 font-weight-bold">
-                <NumberFormat value={account.amountBilled} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+
+              <ClayTable.Cell expanded headingCell align="center" className="table-cell-expand-smallest">
+                {Liferay.Language.get('in-force-policies')}
+                <a href="javascript:;" className="text-muted">
+                  <ClayIcon symbol={"sort"} spritemap={spritemap} />
+                </a>
               </ClayTable.Cell>
+
+              <ClayTable.Cell headingCell align="right" className="table-cell-ws-nowrap">
+                {Liferay.Language.get('amount-billed')}
+                <a href="javascript:;" className="text-muted">
+                  <ClayIcon symbol={"sort"} spritemap={spritemap} />
+                </a>
+              </ClayTable.Cell>
+
             </ClayTable.Row>
-          ))}
-        </ClayTable.Body>
-        <tfoot>
-          <ClayTable.Row>
-            <ClayTable.Cell colSpan="3">
-              <a href="#" className="link-action">{Liferay.Language.get('see-all-accounts')}</a>
-            </ClayTable.Cell>
-          </ClayTable.Row>
-        </tfoot>
-      </ClayTable>
+          </ClayTable.Head>
+          <ClayTable.Body>
+            {props.accountsList.map((account, index) => (
+              <ClayTable.Row key={index}>
+                <ClayTable.Cell>
+                  <h5 className="font-weight-bold mb-0">{account.accountName}</h5>
+                  <small className="text-muted">#{account.accountNumber}</small>
+                </ClayTable.Cell>
+                <ClayTable.Cell align="center">{account.inForcePolicies}</ClayTable.Cell>
+                <ClayTable.Cell align="right" className="h3 font-weight-bold">
+                  <NumberFormat value={account.amountBilled} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                </ClayTable.Cell>
+              </ClayTable.Row>
+            ))}
+          </ClayTable.Body>
+        </ClayTable>
+        <div className="pt-2 px-2"><a href="#" className="link-action">{Liferay.Language.get('see-all-accounts')}</a></div>
+      </React.Fragment>
     );
   }
 };
