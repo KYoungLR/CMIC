0486b194df6c0d4bc7083a73b69e37a622fda297
julienmouraddotcom <62948119+julienmouraddotcom@users.noreply.github.com>
2020-06-15 16:08:45 -0400

    CMC-335 Add policy list UI

---
+++ modules/policy/policy-list-web/src/Policies.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import ClayCard from '@clayui/card';
+import {Toast} from 'com.churchmutual.commons.web';
+import PolicyList from './PolicyList';
+import PolicySearch from './PolicySearch';
+
+class Policies extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      policiesList: [],
+      isLoading: true,
+      filterBy: '',
+      groupId: 0,
+      searchTerm: '',
+      toast:  {
+        displayType: '',
+        message: '',
+        title: '',
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.getPoliciesList();
+  }
+
+  displayErrorMessage(msg) {
+    this.setState({
+      toast: {
+        displayType: 'danger',
+        message: msg,
+        title: Liferay.Language.get('error-colon')
+      }
+    });
+  }
+
+  onToastClosed() {
+    this.setState({
+      toast: {
+        displayType: '',
+        message: '',
+        title: ''
+      }
+    });
+  }
+
+  getPoliciesList() {
+    /*fetch(`/o/account/accounts/${this.userId}`)
+      .then(res => res.json())
+      .then(data => {
+        let accounts = data;
+        this.setState({accountsList: accounts, isLoading: false});
+      })
+      .catch(() => this.displayErrorMessage('error.unable-to-retrieve-list-of-accounts'))*/
+
+    this.setState({
+      policiesList: [
+        { policyName: 'Commercial Auto', policyNumber: '1AJNS1981', startDate: '1/1/20', endDate: '1/1/21', transactions: 7, amountBilled: 54428.21 },
+        { policyName: 'General Liability', policyNumber: '9811ANS1J', startDate: '1/1/20', endDate: '1/1/21', transactions: 1, amountBilled: 27594.21 },
+        { policyName: 'International Travel', policyNumber: '8BCU91982', startDate: '1/1/20', endDate: '1/1/21', transactions: 14, amountBilled: 16750.21 },
+        { policyName: 'Multi-Peril', policyNumber: '44SD1NZC34', startDate: '1/1/19', endDate: '1/1/20', transactions: 35, amountBilled: 2460.21 }
+      ],
+      isLoading: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="account-list-portlet">
+        <ClayCard>
+          <div className="card-header flex-container flex-container-stacked-xs justify-content-between align-items-md-center">
+            <ClayCard.Description displayType="title">{Liferay.Language.get('policies')}</ClayCard.Description>
+            <div className="mt-4 mt-md-0">
+              <PolicySearch
+                isLoading={this.state.isLoading}
+              />
+            </div>
+          </div>
+          <ClayCard.Body>
+            <PolicyList
+              policiesList={this.state.policiesList}
+              isLoading={this.state.isLoading}
+            />
+          </ClayCard.Body>
+        </ClayCard>
+
+        <Toast
+          displayType={this.state.toast.displayType}
+          message={this.state.toast.message}
+          onClose={() => this.onToastClosed()}
+          title={this.state.toast.title} />
+      </div>
+    );
+  }
+};
+
+
+export default Policies;
---
+++ modules/policy/policy-list-web/src/PolicyList.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import ClayButton from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import ClayLoadingIndicator from '@clayui/loading-indicator';
+import ClayTable from '@clayui/table';
+import NumberFormat from 'react-number-format';
+
+const PolicyList = (props) => {
+  const spritemap = Liferay.ThemeDisplay.getPathThemeImages() + '/cmic/icons.svg';
+
+  if (props.isLoading) {
+    return (<ClayLoadingIndicator />);
+  }
+  else {
+    return (
+      <ClayTable className="table-heading-nowrap">
+        <ClayTable.Head>
+          <ClayTable.Row>
+            <ClayTable.Cell expanded headingCell>
+              {Liferay.Language.get('type')}
+              <a href="javascript:;" className="text-muted">
+                <ClayIcon symbol={"sort"} spritemap={spritemap} />
+              </a>
+            </ClayTable.Cell>
+
+            <ClayTable.Cell headingCell>
+              {Liferay.Language.get('effective-date')}
+              <a href="javascript:;" className="text-muted">
+                <ClayIcon symbol={"sort"} spritemap={spritemap} />
+              </a>
+            </ClayTable.Cell>
+
+            <ClayTable.Cell headingCell align="right">
+              {Liferay.Language.get('transactions')}
+              <a href="javascript:;" className="text-muted">
+                <ClayIcon symbol={"sort"} spritemap={spritemap} />
+              </a>
+            </ClayTable.Cell>
+
+            <ClayTable.Cell headingCell align="right">
+              {Liferay.Language.get('amount-billed')}
+              <a href="javascript:;" className="text-muted">
+                <ClayIcon symbol={"sort"} spritemap={spritemap} />
+              </a>
+            </ClayTable.Cell>
+
+            <ClayTable.Cell headingCell />
+
+          </ClayTable.Row>
+        </ClayTable.Head>
+        <ClayTable.Body>
+          {props.policiesList.map((policy, index) => (
+            <ClayTable.Row key={index}>
+              <ClayTable.Cell>
+                <h5 className="font-weight-bold mb-0">{policy.policyName}</h5>
+                <small className="text-muted">#{policy.policyNumber}</small>
+              </ClayTable.Cell>
+
+              <ClayTable.Cell>{policy.startDate} - {policy.endDate}</ClayTable.Cell>
+              <ClayTable.Cell align="right">{policy.transactions}</ClayTable.Cell>
+
+              <ClayTable.Cell align="right" className="h3 font-weight-bold">
+                <NumberFormat value={policy.amountBilled} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+              </ClayTable.Cell>
+
+              <ClayTable.Cell align="right">
+                <ClayButton displayType="unstyled" monospaced className="text-primary">
+                  <ClayIcon symbol={"download"} spritemap={spritemap} className="lexicon-icon-lg" />
+                </ClayButton>
+              </ClayTable.Cell>
+            </ClayTable.Row>
+          ))}
+        </ClayTable.Body>
+      </ClayTable>
+    );
+  }
+};
+
+export default PolicyList;
