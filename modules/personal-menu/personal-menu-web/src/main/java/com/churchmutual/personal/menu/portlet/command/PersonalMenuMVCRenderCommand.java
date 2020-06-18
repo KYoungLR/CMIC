c38079911daed1014535dd09b0222cbd20d0acec
Kayleen Lim <kayleen.lim@liferay.com>
2020-06-18 12:12:13 -0700

    CMIC-344 Retain impersonation info for personal menu link

--- modules/personal-menu/personal-menu-web/src/main/java/com/churchmutual/personal/menu/portlet/command/PersonalMenuMVCRenderCommand.java
+++ modules/personal-menu/personal-menu-web/src/main/java/com/churchmutual/personal/menu/portlet/command/PersonalMenuMVCRenderCommand.java
@@ -3,7 +3,11 @@ package com.churchmutual.personal.menu.portlet.command;
 import com.churchmutual.core.service.CMICUserLocalService;
 import com.churchmutual.personal.menu.constants.PersonalMenuPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -32,6 +36,13 @@ public class PersonalMenuMVCRenderCommand implements MVCRenderCommand {
 	}
 
 	private String _getProfilePageRedirect(RenderRequest renderRequest) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+		if (themeDisplay.isImpersonated()) {
+			return PROFILE_PAGE_REDIRECT + "?doAsUserId=" + HtmlUtil.escapeURL(themeDisplay.getDoAsUserId());
+		}
+
 		return PROFILE_PAGE_REDIRECT;
 	}
 
