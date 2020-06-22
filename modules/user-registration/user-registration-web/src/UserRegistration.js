b3961e4b830fb6f3c51f2fe3205eef40666eb607
julienmouraddotcom <62948119+julienmouraddotcom@users.noreply.github.com>
2020-06-22 14:56:32 -0400

    CMIC-355 Refactor User Registration UI

--- modules/user-registration/user-registration-web/src/UserRegistration.js
+++ modules/user-registration/user-registration-web/src/UserRegistration.js
@@ -1,5 +1,6 @@
-import ClayButton from '@clayui/button';
 import React from 'react';
+import ClayButton from '@clayui/button';
+import ClayLayout from '@clayui/layout';
 import {Input} from 'com.churchmutual.commons.web';
 
 class UserRegistration extends React.Component {
@@ -166,80 +167,92 @@ class UserRegistration extends React.Component {
 
   registrationCodeForm() {
     return (
-      <div className="user-registration-portlet">
-        <h1 className="mb-5">{Liferay.Language.get("enter-registration-code")}</h1>
+      <React.Fragment>
+        <ClayLayout.SheetHeader>
+          <h1 className="sheet-title">{Liferay.Language.get("enter-registration-code")}</h1>
+        </ClayLayout.SheetHeader>
 
-        <form ref={this.formRegistrationCode} onSubmit={(e) => this.onFormRegistrationCodeSubmit(e)}>
+        <ClayLayout.SheetSection>
+          <form ref={this.formRegistrationCode} onSubmit={(e) => this.onFormRegistrationCodeSubmit(e)}>
 
-          <Input
-            fieldName="registrationCode"
-            handleFieldChange={(fieldName, fieldValue) => this.setRegistrationCode(fieldName, fieldValue)}
-            label={Liferay.Language.get("registration-code")}
-            maxLength="150"
-            value={this.state.registrationCode}
-            showErrors={this.state.formErrors.registrationCode}
-            errorMsg={Liferay.Language.get("error.registration-code")}
-          />
+            <Input
+              fieldName="registrationCode"
+              handleFieldChange={(fieldName, fieldValue) => this.setRegistrationCode(fieldName, fieldValue)}
+              label={Liferay.Language.get("registration-code")}
+              maxLength="150"
+              value={this.state.registrationCode}
+              showErrors={this.state.formErrors.registrationCode}
+              errorMsg={Liferay.Language.get("error.registration-code")}
+            />
 
-          <div className="ml-3 mb-5 small">
-            <a data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample"
-               className="collapsed text-muted" href="#helpText">
-              {Liferay.Language.get("cant-find-your-registration-code")}
-            </a>
+            <div className="ml-3 mb-5 small">
+              <a data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample"
+                 className="collapsed text-muted" href="#helpText">
+                {Liferay.Language.get("cant-find-your-registration-code")}
+              </a>
 
-            <div className="collapse" id="helpText">
-              {Liferay.Language.get("registration-help-text")}
+              <div className="collapse" id="helpText">
+                {Liferay.Language.get("registration-help-text")}
+              </div>
             </div>
-          </div>
-          <ClayButton disabled={!this.isFormRegistrationCodeValid()} displayType="primary" type="submit">
-            {Liferay.Language.get("next")}
-          </ClayButton>
-        </form>
-      </div>
+            <ClayButton disabled={!this.isFormRegistrationCodeValid()} displayType="primary" type="submit">
+              {Liferay.Language.get("next")}
+            </ClayButton>
+          </form>
+        </ClayLayout.SheetSection>
+      </React.Fragment>
     );
   }
 
   identityForm() {
     return (
-      <div className="user-registration-portlet">
-        <h1 className="mb-5">{Liferay.Language.get("confirm-your-identity")}</h1>
+      <React.Fragment>
+        <ClayLayout.SheetHeader>
+          <h1 className="sheet-title">{Liferay.Language.get("confirm-your-identity")}</h1>
+        </ClayLayout.SheetHeader>
 
-        <form ref={this.formIdentity} onSubmit={(e) => this.onFormIdentitySubmit(e)}>
-          <Input
-            fieldName="divisionAgentNumber"
-            handleFieldChange={(fieldName, fieldValue) => this.setDivisionAgentNumber(fieldName, fieldValue)}
-            label={Liferay.Language.get("division-agent-number")}
-            maxLength="150"
-            value={this.state.divisionAgentNumber}
-            showErrors={this.state.formErrors.divisionAgentNumber}
-            errorMsg={Liferay.Language.get("error.division-agent-number")}
-            errorMsgPosition="top"
-          />
+        <ClayLayout.SheetSection>
+          <form ref={this.formIdentity} onSubmit={(e) => this.onFormIdentitySubmit(e)}>
+            <Input
+              fieldName="divisionAgentNumber"
+              handleFieldChange={(fieldName, fieldValue) => this.setDivisionAgentNumber(fieldName, fieldValue)}
+              label={Liferay.Language.get("division-agent-number")}
+              maxLength="150"
+              value={this.state.divisionAgentNumber}
+              showErrors={this.state.formErrors.divisionAgentNumber}
+              errorMsg={Liferay.Language.get("error.division-agent-number")}
+              errorMsgPosition="top"
+            />
 
-          <Input
-            fieldName="businessZipCode"
-            handleFieldChange={(fieldName, fieldValue) => this.setStateField(fieldName, fieldValue)}
-            label={Liferay.Language.get("business-zip-code")}
-            maxLength="12"
-            value={this.state.businessZipCode}
-            showErrors={this.state.formErrors.businessZipCode}
-          />
+            <Input
+              fieldName="businessZipCode"
+              handleFieldChange={(fieldName, fieldValue) => this.setStateField(fieldName, fieldValue)}
+              label={Liferay.Language.get("business-zip-code")}
+              maxLength="12"
+              value={this.state.businessZipCode}
+              showErrors={this.state.formErrors.businessZipCode}
+            />
 
-          <ClayButton disabled={!this.isFormIdentityValid()} displayType="primary" type="submit">
-            {Liferay.Language.get("finish")}
-          </ClayButton>
-        </form>
-      </div>
+            <ClayButton disabled={!this.isFormIdentityValid()} displayType="primary" type="submit">
+              {Liferay.Language.get("finish")}
+            </ClayButton>
+          </form>
+        </ClayLayout.SheetSection>
+      </React.Fragment>
     );
   }
 
   render() {
-    if (!this.state.isFormRegistrationCodeSubmitted) {
-      return this.registrationCodeForm();
-    }
-    else {
-      return this.identityForm();
-    }
+    return (
+      <div className="user-registration-portlet">
+        <ClayLayout.Sheet>
+          {!this.state.isFormRegistrationCodeSubmitted
+            ? this.registrationCodeForm()
+            : this.identityForm()
+          }
+        </ClayLayout.Sheet>
+      </div>
+    );
   }
 }
 
